ba948f91da630bf213873892d13f809373ac3f03
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2020-08-11 22:07:55 -0400

    empty div

--- dental-buddy/src/components/ExploreContainer.tsx
+++ dental-buddy/src/components/ExploreContainer.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ExploreContainer.css';
+import mobiscroll from '@mobiscroll/react';
+import '@mobiscroll/react/dist/css/mobiscroll.min.css';
 
 interface ContainerProps {
-  name: string;
+    name: string;
 }
 
-const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
-  return (
-    <div className="container">
-      <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
-  );
+const ExploreContainer: React.FC<ContainerProps> = ({name}) => {
+
+    return (
+
+    )
 };
 
 export default ExploreContainer;
